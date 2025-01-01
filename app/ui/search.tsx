b5e15ca187d8c9936114bd66cca9e2ba07378a10
haphizi <61571184+haphizi@users.noreply.github.com>
2025-01-01 16:27:07 +0800

    Add multiple features and improvements
    - Set up authentication with AUTH_SECRET
    - Add environment variables for database connection
    - Implement search functionality in invoices page
    - Add CreateInvoice component
    - Use Suspense for loading states in Table component
    - Add Search component for searching invoices
    - Add Table component to display invoices
    - Add InvoicesTableSkeleton for loading states
    - Update styles for better responsiveness
    - Set page metadata for invoices page

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,28 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useDebouncedCallback } from "use-debounce";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  // URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters. Instead of creating a complex string literal, you can use it to get the params string like ?page=1&query=a.
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +31,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        // to ensure the input field is in sync with the URL and will be populated when sharing, you can pass a defaultValue to input by reading from searchParams
+
+        // If you're using state to manage the value of an input, you'd use the value attribute to make it a controlled component. This means React would manage the input's state.However, since you're not using state, you can use defaultValue. This means the native input will manage its own state.
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
